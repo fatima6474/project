28ff4040e325335c1ab74e512346b79207e3bdf2
fatima6474 <fateemaah5@gmail.com>
2024-01-18 21:53:20 +0100

    file structure

--- Backend/app.js
+++ Backend/app.js
@@ -26,21 +26,18 @@ require('dotenv').config();
 //   password: "6474",
 //   port: 5432,
 // });
-// const pool = new Pool({
-
-//   PGHOST: 'ep-old-unit-a2zzakiw.eu-central-1.aws.neon.tech',
-//   PGDATABASE: 'skill_community',
-//   PGUSER: 'fatima6474',
-//   PGPASSWORD: 'OHjLTRYkG36P'
-//   // user: "postgres",
-//   // host: "localhost",
-//   // database: "postgres",
-//   // password: "6474",
-//   // port: 5432,
-// });
 const pool = new Pool({
-  connectionString: 'postgresql://fatima6474:OHjLTRYkG36P@ep-old-unit-a2zzakiw.eu-central-1.aws.neon.tech:5432/skill_community?ssl=true',
-  
+
+  PGHOST: 'ep-old-unit-a2zzakiw.eu-central-1.aws.neon.tech',
+  PGDATABASE: 'skill_community',
+  PGUSER: 'fatima6474',
+  PGPASSWORD: 'OHjLTRYkG36P'
+  `sslmode=require`
+  // user: "postgres",
+  // host: "localhost",
+  // database: "postgres",
+  // password: "6474",
+  // port: 5432,
 });
 
 const corsOptions = {
