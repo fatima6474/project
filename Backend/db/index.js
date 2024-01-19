c29fd2f112f0babae85643afa11003c9f55518d2
fatima6474 <fateemaah5@gmail.com>
2024-01-19 08:52:57 +0100

    file structure

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,14 +1,14 @@
-const {Client} = require("pg");
-require("dotenv").config()
-console.log();
-const client = new Client({
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: 5432
-});
+// const {Client} = require("pg");
+// require("dotenv").config()
+// console.log();
+// const client = new Client({
+//     user: process.env.DB_USER,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     password: process.env.DB_PASSWORD,
+//     port: 5432
+// });
 
-client.connect();
+// client.connect();
 
-module.exports = client;
+// module.exports = client;
