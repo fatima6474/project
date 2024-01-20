b77c3a9c08847c702e05f0c890f55b33c5e551cb
fatima6474 <fateemaah5@gmail.com>
2024-01-20 11:43:33 +0100

    file structure

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -13,15 +13,3 @@
 
 // module.exports = client;
 
-const client = new pool({
-    user:   process.env.DB_USER,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-
-   ssl: {
-    rejectUnauthorized: false,
-   },
-   sslmode: 'require'
-});
