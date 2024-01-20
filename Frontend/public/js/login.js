c53cf1cc5c4e6c4ccd8a224ac6e47f998268c244
fatima6474 <fateemaah5@gmail.com>
2024-01-20 15:20:13 +0100

    file structure

--- Frontend/public/js/login.js
+++ Frontend/public/js/login.js
@@ -1,25 +1,23 @@
 const email = document.querySelector('#email'),
-      password = document.querySelector('#password'),
-      buttonBtn = document.querySelector('#submitBtn'),
-      emailErrorMsg = document.querySelector('#emailErrorMessage'),
-      passwordErrorMsg = document.querySelector('#passwordErrorMessage'),
-      emailPattern =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-      passwordPattern =   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@#()^$!%*?&]{8,}$/;
-      const baseURL = "https://skillworkcommunity.onrender.com/";
+    password = document.querySelector('#password'),
+    buttonBtn = document.querySelector('#submitBtn'),
+    emailErrorMsg = document.querySelector('#emailErrorMessage'),
+    passwordErrorMsg = document.querySelector('#passwordErrorMessage'),
+    emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@#()^$!%*?&]{8,}$/;
+const baseURL = "https://skillworkcommunity.onrender.com/";
 
-      buttonBtn.addEventListener('click', async (e) => {
-          e.preventDefault();
-      
-          if (emailValidation() && passwordValidation()) {
-              let userData = {
-                  email: email.value.trim(),
-                  password: password.value,
-              };
-              console.log("line 23");
-              loginPost(`${baseURL}login`, userData);
-          }
-      // });
-      
+buttonBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    if (emailValidation() && passwordValidation()) {
+        let userData = {
+            email: email.value.trim(),
+            password: password.value,
+        };
+        console.log("line 23");
+        loginPost(`${baseURL}login`, userData);
+    }
       // Rest of your code...
       
 
