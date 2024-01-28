9b37a083e2ad5f340b5dcb95c3bf6d31ae5080ea
fatima6474 <fateemaah5@gmail.com>
2024-01-28 19:59:33 +0100

    file structure

--- Backend/app.js
+++ Backend/app.js
@@ -443,10 +443,10 @@ app.post('/api/text', async (req, res) => {
   try {
     const { senderEmail, receiverEmail, jobDescription, messageText } = req.body;
     const insertTextQuery = `
-      INSERT INTO text (sender_email, receiver_email, job_description, message_text)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO text (receiver_email, job_description, message_text)
+      VALUES ($1, $2, $3)
     `;
-    const values = [senderEmail, receiverEmail, jobDescription, messageText];
+    const values = [receiverEmail, jobDescription, messageText];
     await pool.query(insertTextQuery, values);
     res.status(201).send('Text sent successfully');
   } catch (error) {
